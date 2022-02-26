30caa123a42ecd49c74b79622f4628f3ffdc7244
ilguy <yostr01@yahoo.com>
2022-02-25 19:36:36 -0600

    Made it through chapter 3.6

--- Chapter 1.R
+++ Chapter 1.R
@@ -1,3 +1,47 @@
 # Chapter 1 Code and notes
 library(tidyverse)
 tidyverse_update()
+
+mpg
+?mpg
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = cyl, y = hwy))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = class, y = drv))
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy, color = class))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, size = class))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, shape = class))
+
+# Notice how color goes outside aes here
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
+
+mpg
+
+# Shading when color is a continuous variable
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = cty))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, size = cty))
+
+# Cannot map shape to continuous variable
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, shape = cty))
+
+?geom_point
+vignette("ggplot2-specs")
