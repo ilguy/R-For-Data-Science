76fc19cc90814627c4d7f3723ff700ffe08604d1
ilguy <yostr01@yahoo.com>
2022-03-26 14:08:06 -0500

    Commit first of Chapter 11

---
+++ Chapter 11.R
@@ -0,0 +1,21 @@
+# Chapter 11 Data Import
+library(tidyverse)
+
+heights <- read_csv("https://raw.githubusercontent.com/hadley/r4ds/main/data/heights.csv")
+
+read_csv("a,b,c
+         1,2,3
+         4,5,6")
+
+read_csv("The first line of metadata
+         The second line of metadata
+         x,y,z
+         1,2,3", skip = 2)
+
+read_csv("1,2,3\n4,5,6", col_names = FALSE)
+
+read_csv("1,2,3\n4,5,6", col_names = c("x", "y", "z"))
+
+read_csv("a,b,c\n1,2,.", na = ".")
+
+#11.3
