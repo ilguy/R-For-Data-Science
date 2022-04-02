27a79e47bac7c69f50996de3d0312e328c30ccee
ilguy <yostr01@yahoo.com>
2022-04-02 07:55:28 -0500

    Final commit for Chapter 11

--- Chapter 11.R
+++ Chapter 11.R
@@ -39,3 +39,72 @@ parse_character(x2, locale = locale(encoding = "Shift-JIS"))
 
 guess_encoding(charToRaw(x1))
 guess_encoding(charToRaw(x2))
+
+# Factors
+fruit <- c("apple", "banana")
+parse_factor(c("apple", "banana", "bananana"), levels = fruit)
+
+parse_datetime("2010-10-01T2010")
+parse_datetime("20101010")
+
+parse_date("2010-10-01")
+
+library(hms)
+parse_time("01:10 am")
+parse_time("20:10:01")
+
+parse_date("01/02/15", "%m/%d/%y")
+
+parse_date("1 janvier 2015", "%d %B %Y", locale = locale("fr"))
+
+guess_parser("2010-10-01")
+
+challenge <- read_csv(readr_example("challenge.csv"))
+
+problems(challenge)
+tail(challenge)
+
+challenge <- read_csv(readr_example("challenge.csv"),
+                      col_types = cols(
+                        x = col_double(),
+                        y = col_logical()
+                      ))
+challenge <- read_csv(
+  readr_example("challenge.csv"), 
+  col_types = cols(
+    x = col_double(),
+    y = col_date()
+  )
+)
+
+tail(challenge)
+
+challenge2 <- read_csv(readr_example("challenge.csv"), guess_max = 1001)
+challenge2
+
+challenge2 <- read_csv(readr_example("challenge.csv"),
+                       col_types = cols(.default = col_character()))
+
+df <- tribble(
+  ~x, ~y,
+  "1", "1.21",
+  "2", "2.32",
+  "3", "4.56"
+)
+df
+
+type_convert(df)
+
+write_csv(challenge, "challenge.csv")
+
+challenge
+
+write_csv(challenge, "challenge-2.csv")
+read_csv("challenge-2.csv")
+
+write_rds(challenge, "challenge.rds")
+read_rds("challenge.rds")
+
+library(feather)
+write_feather(challenge, "challenge.feather")
+read_feather("challenge.feather")
